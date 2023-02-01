3fc806308428df5f20160399dcb053b177448667
Zorous <oussamabelhadi5@gmail.com>
2023-02-01 22:25:26 +0100

    Customizing Sanity

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -1,8 +1,13 @@
 import {defineCliConfig} from 'sanity/cli'
 
+
+
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
+
 export default defineCliConfig({
   api: {
-    projectId: 'y3h7iy2t',
-    dataset: 'production'
+    projectId,
+    dataset
   }
 })
