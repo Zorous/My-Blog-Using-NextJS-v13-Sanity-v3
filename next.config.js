ef166b05f67b6bf8db070b6ea7a2703a196f4aa9
Zorous <oussamabelhadi5@gmail.com>
2023-02-01 17:19:10 +0100

    init

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  //this allows us to use a new app directory wallet
+  experimental : {
+    appDir : true
+  },
 }
