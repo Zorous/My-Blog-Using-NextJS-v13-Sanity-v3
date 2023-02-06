c3701e913c0e3602ff17eb08bc4d57c0dc95ae11
Zorous <oussamabelhadi5@gmail.com>
2023-02-06 21:29:45 +0100

    debugging blog section (✔)

--- components/BlogList.tsx
+++ components/BlogList.tsx
@@ -1,22 +1,26 @@
 import Image from "next/image";
+// import { use, useEffect, useState } from "react";
 import urlFor from "../lib/urlFor";
+import NotFoundImg from "../assets/notfound.png";
 
 type Props = {
     posts : Post[];
 }
 function BlogList({posts} : Props) {
-    console.log(posts.length);
+// const [postsS,setPostsS] = useState();
+    console.log(urlFor(posts[1].mainImage).url());
+    // useEffect(()=>setPostsS(posts),[posts]);
   return (
     <div>
         <hr className="border-[#0fbcf9]"/>
 
         <div>
-            {posts? posts.map(post =>(
-                <div key={post._id}>
+        {posts? posts.map(post =>(
+                <div key={post?._id}>
                     <div className="relative w-full h-80 drop-shadow-xl group-hover:scale-105 transition-transform duration-200 ease-out">
                         <Image className = "object-cover object-left lg-object-center"
-                        src = {urlFor(post.mainImage).url()}
-                        alt={post.author.name}
+                        src = {post.mainImage?urlFor(post.mainImage).url():NotFoundImg}
+                        alt={post.author?.name}
                         fill
                         />
                     </div>
