3cd1d9660a632d68a6aa3877965a86d653423a48
Zorous <oussamabelhadi5@gmail.com>
2023-02-08 02:21:24 +0100

    Blog body serialization (2/2) ✔

--- components/RichTextComponent.tsx
+++ components/RichTextComponent.tsx
@@ -2,19 +2,62 @@ import Link from "next/link";
 import Image from "next/image";
 import urlFor from "../lib/urlFor";
 
-export const RichTextComponent = ()=>{
+export const RichTextComponent = {
 types : {
-    image : ({value} : any) =>{
-        return(
+    image : ({value} : any) => {
+        return (
             <div className="">
                 <Image
                 className="object-contain"
                 src={urlFor(value).url()}
                 alt="Blog Post Image"
+                fill
                 />
             </div>
-        )
+        );
     }
+},
+list : {
+    bullet : ({children} : any) => (
+        <ul className="ml-10 py-5 list-disc space-y-5">{children}</ul>
+    ),
+    number : ({children} : any) => (
+        <ol className="mt-lg list-decimal">{children}</ol>
+    )
+},
+block : {
+    h1: ({children} : any) => (
+        <h1 className="text-5xl py-10 font-bold">{children}</h1>
+    ),
+    h2: ({children} : any) => (
+        <h1 className="text-4xl py-10 font-bold">{children}</h1>
+    ),
+    h3: ({children} : any) => (
+        <h1 className="text-3xl py-10 font-bold">{children}</h1>
+    ),
+    h4: ({children} : any) => (
+        <h1 className="text-2xl py-10 font-bold">{children}</h1>
+)},
+blockquote : ({children} : any) => (
+    <blockquote className="border-l-[#0fbcf9] border-l-4 pl-5 py-5 my-5">
+        {children}
+    </blockquote>
+),
+marks : {
+    link : ({children, value} : any) => {
+        const rel = !value.href.startsWith('/')?"noreferrer noopener" : undefined;
+    
+    return (
+        <Link
+        href={value.href}
+        rel={rel}
+        className='underline decoration-[#0fbcf9] hover:decoration-black'
+        >
+            {children}
+        </Link>
+    );
 }
 
+
+}
 }
